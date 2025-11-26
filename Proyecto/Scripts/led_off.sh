c5665c94d94559e4f8efffad4eaa655aae6b3c81
AnitaGuillen <ana.guillen@iteso.mx>
2025-11-26 16:09:04 -0600

    Avances S15

--- Proyecto/Scripts/led_off.sh
+++ Proyecto/Scripts/led_off.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+
+STATE_LED="/tmp/led0_state"
+echo 0 > "$STATE_LED"
+
+# Apagar LED0 (LEDOUT0 = 0b00)
 i2cset -y 2 0x60 0x02 0x00
--- Proyecto/Scripts/led_on.sh
+++ Proyecto/Scripts/led_on.sh
@@ -1,9 +1,14 @@
+#!/bin/sh
+
+STATE_LED="/tmp/led0_state"
+echo 1 > "$STATE_LED"
+
 # Despertar y modo normal
 i2cset -y 2 0x60 0x00 0x00      # MODE1: SLEEP=0
-i2cset -y 2 0x60 0x01 0x04      # MODE2: normal 
+i2cset -y 2 0x60 0x01 0x04      # MODE2: normal
 
 # Asegurar corriente en el canal 0
-i2cset -y 2 0x60 0x40 0xFF      
+i2cset -y 2 0x60 0x40 0xFF
 
-# Poner LED0 en modo PWM individual (LEDOUT0 bits [1:0] = 10b)
-i2cset -y 2 0x60 0x02 0x02      # LEDOUT0 = 0b00000010
+# Poner LED0 en modo PWM individual
+i2cset -y 2 0x60 0x02 0x02
